3ae1b7de4a8784f40e1cf44a175316c96ec383fc
ZABETH <karta1862025>
2025-08-23 00:10:48 +0800

    [Add] Setting for deploy GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), tailwindcss()],
+  base: '/zabeth-portfolio/',
+  build: { outDir: 'docs' },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
