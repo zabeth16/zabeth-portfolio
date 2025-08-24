0aaf0eb0691c81e6b2f065b5122e78a8d4737aea
ZABETH <karta1862025>
2025-08-24 23:49:52 +0800

    [Revise] Optimize RWD animate: skill cards.

--- src/views/HomeView/component/SkillCards.vue
+++ src/views/HomeView/component/SkillCards.vue
@@ -91,7 +91,7 @@ onMounted(() => {
         opacity: 1,
         scrollTrigger: {
           trigger: card,
-          start: 'top 30%',
+          start: 'top 60%',
           end: 'top 60%',
           toggleActions: 'play none none reverse',
           refreshPriority: -1, // 降低優先級，避免過於頻繁的計算
